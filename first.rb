ec81bc3c3614fdcd5ebd5e693e1be7a81c437197
23gjk02 <k23gjk02@st.kyusan-u.ac.jp>
2023-05-08 16:42:48 +0900

    四則演算と条件分岐

--- first.rb
+++ first.rb
@@ -2,7 +2,20 @@ puts "Hello"
 puts "KSU"
 # put string
 
-i = 5
-i = 3
+i = 8
 s = "KSU"
 puts s + i.to_s
+
+j = 2.0
+
+puts i + j
+puts i - j
+puts i * j
+puts i / j
+puts i % j
+
+if i % 2 == 0
+    puts "even"
+else
+    puts "odd"
+end
