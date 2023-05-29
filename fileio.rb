a7ef9a4b4109a483029f71a77532f4532d50fbae
23gjk02 <k23gjk02@st.kyusan-u.ac.jp>
2023-05-29 16:36:11 +0900

    open/closeをfile.openに変更

--- fileio.rb
+++ fileio.rb
@@ -1,8 +1,7 @@
-f = File.open "ls.out","r"
-i = 1
-while line = f.gets
-    puts i.to_s + ": " + line
-    i += 1
+File.open("ls.out","r") do |f|
+    i = 1
+    while line = f.gets
+        puts i.to_s + ": " + line
+        i += 1
+    end
 end
-
-f.close
