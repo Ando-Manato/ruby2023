6b429c0afa2eda4c0cd3b1b59f4015421e4690eb
23gjk02 <k23gjk02@st.kyusan-u.ac.jp>
2023-06-05 17:16:32 +0900

    -nオプションを実装

--- cat.rb
+++ cat.rb
@@ -1,5 +1,16 @@
-File.open(ARGV[0],"r") do |file|
-    while line = file.gets
-        puts line
+if ARGV[0] == "-n"
+    File.open(ARGV[1],"r") do |file|
+        i = 1
+        while line = file.gets
+            puts i.to_s + ": " + line
+            i += 1
+        end
+    end
+else
+    File.open(ARGV[0],"r") do |file|
+        while line = file.gets
+            puts line
+        end
     end
 end
+
